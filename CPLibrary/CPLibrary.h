7d8e2ad5ec26c5235e202de6a777b38a143db564
ColinDev <colinpham.cp@gmail.com>
2025-11-30 16:59:30 +0100

    Some changes

--- CPLibrary/CPLibrary.h
+++ CPLibrary/CPLibrary.h
@@ -1,17 +1,17 @@
 #pragma once
 
 #include <glad/glad.h>
-#include <GLFW/glfw3.h>
+
 #include "../CPLibrary/Audio.h"
-#include "../CPLibrary/shapes2D/Triangle.h"
-#include "../CPLibrary/shapes2D/Circle.h"
-#include "../CPLibrary/shapes2D/Rectangle.h"
-#include "../CPLibrary/shapes2D/Line.h"
-#include "../CPLibrary/shapes2D/Texture2D.h"
-#include "../CPLibrary/shapes2D/PointLight.h"
-#include "../CPLibrary/shapes2D/ScreenQuad.h"
-#include "../CPLibrary/timers/TimerManager.h"
+#include "../CPLibrary/CPL.h"
 #include "../CPLibrary/Shader.h"
 #include "../CPLibrary/Text.h"
-#include "../CPLibrary/CPL.h"
-#include "../CPLibrary/Audio.h"
+#include "../CPLibrary/shapes2D/Circle.h"
+#include "../CPLibrary/shapes2D/Line.h"
+#include "../CPLibrary/shapes2D/PointLight.h"
+#include "../CPLibrary/shapes2D/Rectangle.h"
+#include "../CPLibrary/shapes2D/ScreenQuad.h"
+#include "../CPLibrary/shapes2D/Texture2D.h"
+#include "../CPLibrary/shapes2D/Triangle.h"
+#include "../CPLibrary/timers/TimerManager.h"
+#include <GLFW/glfw3.h>
