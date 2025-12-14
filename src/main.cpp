b7fb300d4aebb52ef0e0421a866a88a0488a8bfd
ColinDev <colinpham.cp@gmail.com>
2025-12-14 17:14:33 +0100

    Formatted code

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "../CPLibrary/CPLibrary.h"
 #ifdef __EMSCRIPTEN__
-    #include <emscripten/emscripten.h>
+#include <emscripten/emscripten.h>
 #endif
 
 using namespace CPL;
@@ -12,7 +12,7 @@ std::vector<PointLight> lights;
 void MainLoop() {
     // Update framework (fps, delta time, input etc.)
     UpdateCPL();
-    
+
     // Start post processing (everything after is affected by PP)
     BeginPostProcessing();
 
@@ -20,7 +20,7 @@ void MainLoop() {
     ClearBackground(BLACK);
 
     // Start drawing shapes affected by lighting
-    BeginDrawing(SHAPE_2D_LIGHT, false); 
+    BeginDrawing(SHAPE_2D_LIGHT, false);
 
     // Set ambient strength
     SetAmbientLight(0.0f);
@@ -36,15 +36,15 @@ void MainLoop() {
 
     // Start drawing text
     BeginDrawing(TEXT, false);
-	
+
     ShowDetails();
-    
+
     // End PP (everything after is not affected)
     EndPostProcessing();
     // Apply effects (nothing, blur, inversed colors etc.)
     ApplyPostProcessing(PP_DEFAULT);
 
-    // End drawing 
+    // End drawing
     EndDrawing();
 
     // Swap buffers
@@ -55,25 +55,25 @@ void MainLoop() {
 int main() {
     // Create 800x600 window with title
     InitWindow(800, 600, "Welcome to CPL");
-    // Set the window icon
-    #ifndef __EMSCRIPTEN
-        SetWindowIcon("assets/images/logo.png");
-    #endif
+// Set the window icon
+#ifndef __EMSCRIPTEN
+    SetWindowIcon("assets/images/logo.png");
+#endif
 
     // Add point lights to vector
     lights.push_back(PointLight({200, 200}, 450.0f, 2.0f, GREEN));
     lights.push_back(PointLight({500, 300}, 550.0f, 1.0f, RED));
     lights.push_back(PointLight({300, 400}, 350.0f, 2.0f, BLUE));
-    
-    // Set Emscripten main loop if compiling to web else default
-    #ifdef __EMSCRIPTEN__
-        emscripten_set_main_loop(MainLoop, 0, true);
-    #else
-	while (!WindowShouldClose()) {
-	    MainLoop();
-	}
-    #endif       
-    
+
+// Set Emscripten main loop if compiling to web else default
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(MainLoop, 0, true);
+#else
+    while (!WindowShouldClose()) {
+        MainLoop();
+    }
+#endif
+
     // Close window
     CloseWindow();
 }
